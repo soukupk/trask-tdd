04dcc063f3f1cdf2fc3d48ed6c443c8cc4ba4f5f
Soukup Karel <ksoukup@trask.cz>
2017-11-30 10:32:21 +0100

    spring boot project initialization

---
+++ src/main/java/cz/trask/tdd/UsersApplication.java
@@ -0,0 +1,12 @@
+package cz.trask.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+}
